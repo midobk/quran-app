1e44cab83c00990be2a363a197fe44cda0f80e08
midobk <mehdi.bakkalimaassom@gmail.com>
2026-03-04 00:24:15 -0500

    Replace placeholder widget test with app boot test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,22 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:quran_app/core/di/service_locator.dart';
+import 'package:quran_app/ui/app.dart';
 
 void main() {
-  test('legacy widget test placeholder', () {
-    expect(true, isTrue);
+  setUp(() async {
+    await resetDependencies();
+    await setupDependencies();
+  });
+
+  tearDown(() async {
+    await resetDependencies();
+  });
+
+  testWidgets('QuranApp boots to HomeScreen', (WidgetTester tester) async {
+    await tester.pumpWidget(const QuranApp());
+    await tester.pumpAndSettle();
+
+    expect(find.text('Quran Live Ayah'), findsOneWidget);
+    expect(find.text('Start Listening'), findsOneWidget);
   });
 }
